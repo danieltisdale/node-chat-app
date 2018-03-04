0fc715f40fa449e568473d13cae7df30bbf7120c
Daniel Tisdale <daniel.tisdale@merlinlabs.com>
2018-03-04 13:46:08 -0600

    First commit of node chat app

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const express = require('express');
+const publicPath = path.join(__dirname, '../public');
+
+var app = express();
+const port = process.env.port || 3001;
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+    console.log(`Started on port ${port}`);
+});
